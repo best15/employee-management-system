7d66c1c47e0f7aa8332cf015a13fd8a4a488590e
SBC <lochan.sharad@gmail.com>
2021-05-27 00:18:39 +0800

    Change in EMS.js. User prompt create with choices to view employees,add employees added etc added. Code to display employee table when user selects view all employees added.

--- EMS.js
+++ EMS.js
@@ -15,9 +15,58 @@ const connection = mysql.createConnection({
     database: process.env.DB_NAME,
 });
 
+function viewEmployees() {
+    connection.query(
+        `select
+        e1.id,
+        e1.first_name,
+        e1.last_name,
+        role.title,
+        role.salary,
+        department.D_name AS Department,
+        concat(e2.first_name, " ", e2.last_name) as Manager
+        from
+        employee e1
+        INNER JOIN role ON e1.role_id = role.id
+        INNER JOIN department on role.department_id = department.id
+        LEFT JOIN employee e2 on e1.manager_id = e2.id;`,
+        (err, response) => {
+            if (err) throw err;
+            console.table(response);
+        });
+
+};
+
+const showMenu = () => {
+    const question = [
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What do you like to do ?  ',
+            choices: ["View all employees", "View employees by department ?",
+                "View all employees by Role ?", "Add Employee ",
+                "Remove Employee ?", "Update Employee Role", "Update Employee Manager"]
+        }
+    ]
+
+    inquirer.prompt(question).then((userChoice) => {
+        proceedUserChoice(userChoice);
+    });
+
+}
+
+function proceedUserChoice(userChoice) {
+    console.log("userChoice", userChoice);
+    if (userChoice.action === 'View all employees') {
+        viewEmployees();
+        showMenu();
+    }
+    else { console.log('errorr receiving data') };
+
+}
 
 connection.connect((err) => {
     if (err) throw err;
     console.log(`PlaylistDB connected as id ${connection.threadId}`);
-
+    showMenu();
 });
