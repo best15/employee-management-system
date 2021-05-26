9727c0588172cd06fbc757208738afef331661db
SBC <lochan.sharad@gmail.com>
2021-05-26 22:56:41 +0800

    employeedbSeed file updated with select statement to display all required employee details for EMS application.

--- employeedbSeed.sql
+++ employeedbSeed.sql
@@ -24,7 +24,7 @@ CREATE TABLE employee(
 INSERT INTO
   department (D_name)
 VALUES
-  ("HR"),("Sales")("Finance"),
+  ("HR"),("Sales"),("Finance"),
   ("Engineering"),("Legal"),("Marketing");
 INSERT INTO
   role (title, salary, department_id)
@@ -36,8 +36,22 @@ VALUES
 INSERT INTO
   employee (first_name, last_name, role_id, manager_id)
 VALUES
-  ("Mike", "Sonic", 5, 1),("Tony", "Brown", 6, 2),
-  ("Gerrard", "Buter", 7,),("Kroos", "Muller", 1, 4),
+  ("Mike", "Sonic", 5, 7),("Tony", "Brown", 6, 5),
+  ("Gerrard", "Buter", 7, null),("Kroos", "Muller", 1, 1),
   ("Steve", "Hut", 2, 7),("Sadio", "Mane", 3, 5),
-  ("Bobby", "Dylon", 4,),("Tom", "J", 6, 2),
-  ("Kevin", "Jonson", 5,);
+  ("Bobby", "Dylon", 4, null),("Tom", "J", 6, 5),
+  ("Kevin", "Jonson", 5, null);
+select
+select
+  e1.id,
+  e1.first_name,
+  e1.last_name,
+  role.title,
+  role.salary,
+  department.D_name AS Department,
+  concat(e2.first_name, " ", e2.last_name) as Manager
+from
+  employee e1
+  INNER JOIN role ON e1.role_id = role.id
+  INNER JOIN department on role.department_id = department.id
+  JOIN employee e2 on e1.manager_id = e2.id;
